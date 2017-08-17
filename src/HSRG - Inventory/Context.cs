41b2ad98402fdf0672f98d491983fcb08a81e497
Tyler McMorris <tylergagemcmorris@gmail.com>
2017-08-17 10:43:00 -0500

    Added all get-set tables to the Context page.

--- src/HSRG - Inventory/Context.cs
+++ src/HSRG - Inventory/Context.cs
@@ -24,6 +24,13 @@ namespace HSRG___Inventory
         public virtual DbSet<InventoryDetail> InventoryDetails { get; set; }
         public virtual DbSet<BIOSInformation> BIOSInformation { get; set; }
         public virtual DbSet<MemoryInformation> MemoryInformation { get; set; }
+        public virtual DbSet<MemoryInformation> DriveSpace { get; set; }
+        public virtual DbSet<MemoryInformation> HardDiskInformation { get; set; }
+        public virtual DbSet<MemoryInformation> MotherBoardInformation { get; set; }
+        public virtual DbSet<MemoryInformation> NetworkAdapters { get; set; }
+        public virtual DbSet<MemoryInformation> SystemEnclosure { get; set; }
+        public virtual DbSet<MemoryInformation> SystemInformation { get; set; }
+
     }
 
 }
